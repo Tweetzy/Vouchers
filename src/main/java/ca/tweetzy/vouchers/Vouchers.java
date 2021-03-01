724109fed68d23f713a69abc61eced35e0aadbdb
Kiran Hart <kiran.hart@live.com>
2021-03-01 17:22:55 -0500

    initial commit

---
+++ src/main/java/ca/tweetzy/vouchers/Vouchers.java
@@ -0,0 +1,40 @@
+package ca.tweetzy.vouchers;
+
+import ca.tweetzy.core.TweetyPlugin;
+import ca.tweetzy.core.configuration.Config;
+
+import java.util.List;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: March 01 2021
+ * Time Created: 5:15 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public class Vouchers extends TweetyPlugin {
+
+    @Override
+    public void onPluginLoad() {
+
+    }
+
+    @Override
+    public void onPluginEnable() {
+
+    }
+
+    @Override
+    public void onPluginDisable() {
+
+    }
+
+    @Override
+    public void onConfigReload() {
+
+    }
+
+    @Override
+    public List<Config> getExtraConfig() {
+        return null;
+    }
+}
